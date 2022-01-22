a525a9c57f5dfc21e19b363dbde38692b6891e55
Ruslan <RuRaIdrisov@omega.sbrf.ru>
2022-01-22 18:14:51 +0300

    Initial commit

---
+++ src/main/java/ru/idrisov/sbrf/k5m/docx_diff_finder/DocxDiffFinderApplication.java
@@ -0,0 +1,13 @@
+package ru.idrisov.sbrf.k5m.docx_diff_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocxDiffFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocxDiffFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/idrisov/sbrf/k5m/docx_diff_finder/DocxDiffFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.idrisov.sbrf.k5m.docx_diff_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocxDiffFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
